021953c49c7e6b61c8095bd9589c02daef18894a
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2022-12-15 13:06:36 +0300

    complete assignment

--- src/features/todos/CreateTodo.js
+++ src/features/todos/CreateTodo.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { todoAdded } from './todosSlice'
 
 function CreateTodo() {
-  return <div>Create Todo Component</div>;
+  const [todo, setTodo] = useState('')
+  const todos = useSelector((state) => state.todos.entities)
+  const dispatch = useDispatch()
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    // console.log(todo)
+    dispatch(todoAdded(todo))
+    e.target.reset()
+  }
+  return (
+    <div>
+      <form onSubmit={(e) => handleFormSubmit(e)}>
+        <p>
+          <label>add todo</label>
+          <input type='text' onChange={(e) => setTodo(e.target.value)} />
+        </p>
+        <input type='submit' />
+      </form>
+      <div>
+        {todos.map((todo, ind) => (
+          <p key={ind}>{todo}</p>
+        ))}
+      </div>
+    </div>
+  )
 }
 
-export default CreateTodo;
+export default CreateTodo
